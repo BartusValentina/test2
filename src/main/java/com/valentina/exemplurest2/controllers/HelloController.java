d03bc600b616694ac26c24d9cc5b27aace70c321
Valentina Bartus <Valentina.Bartus@endava.com>
2020-09-04 12:59:07 +0300

    am adaugat metoda Ciao

--- src/main/java/com/valentina/exemplurest2/controllers/HelloController.java
+++ src/main/java/com/valentina/exemplurest2/controllers/HelloController.java
@@ -2,6 +2,7 @@ package com.valentina.exemplurest2.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,4 +12,10 @@ public class HelloController {
     String hello(){
         return "Salut";
     }
+
+    @PostMapping ("/ciao")
+    String ciao(){
+        return "Ciao";
+    }
 }
+
