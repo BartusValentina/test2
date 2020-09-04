643c77409db00613ca72dcc6cb0f69276233843b
Valentina Bartus <Valentina.Bartus@endava.com>
2020-09-04 13:08:04 +0300

    holiday

--- src/main/java/com/valentina/exemplurest2/controllers/HelloController.java
+++ src/main/java/com/valentina/exemplurest2/controllers/HelloController.java
@@ -1,6 +1,7 @@
 package com.valentina.exemplurest2.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,11 @@ public class HelloController {
     @PostMapping ("/ciao")
     String ciao(){
         return "Ciao";
+    }
+
+    @DeleteMapping ("/holiday")
+    String holiday (){
+        return "ooo";
     }
 }
 
